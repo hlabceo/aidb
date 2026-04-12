7207df93daa2ea994034d4f144c56faa4d872588
hlabceo <help@hlabsoft.co.kr>
2026-04-12 18:15:59 +0900

    feat: 목욕장업 수집 추가, 목욕탕/사우나/찜질방 검색 동의어 추가

--- backend/services/collector.py
+++ backend/services/collector.py
@@ -29,6 +29,7 @@ SERVICE_MAP = {
     "beauty_salons":  ("미용업",     "https://apis.data.go.kr/1741000/beauty_salons/info"),
     "fitness_centers":("체력단련장", "https://apis.data.go.kr/1741000/fitness_centers/info"),
     "billiard_halls": ("당구장",    "https://apis.data.go.kr/1741000/billiard_halls/info"),
+    "public_baths":   ("목욕장업",  "https://apis.data.go.kr/1741000/public_baths/info"),
     # 아래 API들은 data.go.kr에서 추가 승인 후 주석 해제
     # "general_restaurants": ("일반음식점", "https://apis.data.go.kr/1741000/general_restaurants/info"),
     # "pharmacies":          ("약국",       "https://apis.data.go.kr/1741000/pharmacies/info"),
